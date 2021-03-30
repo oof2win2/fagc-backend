6eebda014204f38b047c7988148a4c998e0b2aae
oof2win2 <23482862+oof2win2@users.noreply.github.com>
2021-03-30 20:01:13 +0200

    Get all revocations & proper HTTP

--- src/routes/revocations.js
+++ src/routes/revocations.js
@@ -3,14 +3,22 @@ const router = express.Router();
 const RevocationModel = require("../database/schemas/revocation")
 
 router.get('/getrevocations', async (req, res) => {
-    if (req.body.playername === undefined || typeof (req.body.playername) !== 'string')
-        return res.status(400).send(`Bad Request: playername expected string, got ${typeof (req.body.playername)} with value of ${req.body.playername}`)
-    if (req.body.communityname === undefined || typeof (req.body.communityname) !== 'string')
-        return res.status(400).send(`Bad Request: communityname expected string, got ${typeof (req.body.communityname)} with value of ${req.body.communityname}`)
-    let revocation = await RevocationModel.find({
-        playername: req.body.playername,
-        communityname: req.body.communityname
+    if (req.query.playername === undefined || typeof (req.query.playername) !== 'string')
+        return res.status(400).json({ error: "Bad Request", description: `playername expected string, got ${typeof (req.body.playername)} with value of ${req.body.playername}`})
+    if (req.query.communityname === undefined || typeof (req.query.communityname) !== 'string')
+        return res.status(400).json({ error: "Bad Request", description: `communityname expected string, got ${typeof (req.body.communityname)} with value of ${req.body.communityname}`})
+    let revocations = await RevocationModel.find({
+        playername: req.query.playername,
+        communityname: req.query.communityname
     })
-    return res.status(200).send(revocation)
+    return res.status(200).json(revocations)
+})
+router.get('/getallrevocations', async (req, res) => {
+    if (req.query.playername === undefined || typeof (req.query.playername) !== 'string')
+        return res.status(400).json({ error: "Bad Request", description: `playername expected string, got ${typeof (req.body.playername)} with value of ${req.body.playername}`})
+    let revocations = await RevocationModel.find({
+        playername: req.query.playername
+    })
+    return res.status(200).json(revocations)
 })
 module.exports = router
