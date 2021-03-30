f511b01a6ec665cdf71960a3e6683ce16945266f
oof2win2 <23482862+oof2win2@users.noreply.github.com>
2021-03-30 19:59:22 +0200

    Endpoint for removal of webhook

--- src/routes/informatics.js
+++ src/routes/informatics.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const WebhookSchema = require("../database/schemas/webhook")
+const LogSchema = require("../database/schemas/log")
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -17,8 +18,32 @@ router.post('/addWebhook', async (req, res) => {
         token: req.body.token,
         description: req.body.description
     })
-    // console.log(dbRes, "result")
     res.status(200).send(dbRes)
+})
+router.delete('/removeWebhook', async (req, res) => {
+    if (req.body.id === undefined || isNaN(req.body.id))    
+        return res.status(400).json({ error: "Bad Request", description: `id expected string, got ${typeof (req.body.id)} with value of ${req.body.id}` })
+    const removed = await WebhookSchema.findOneAndDelete({
+        id: req.body.id,
+        apiKey: req.headers.apikey
+    })
+    res.status(200).json(removed)
+})
+router.get('/getlogs', async (req, res) => {
+    if (req.query.limit === undefined || isNaN(req.query.limit))
+        return res.status(400).json({ error: "Bad Request", description: `limit expected number, got ${typeof(req.query.limit)} with value ${req.query.limit}`})
+    if (req.query.afterDate !== undefined && isNaN (req.query.afterDate))
+        return res.status(400).json({ error: "Bad Request", description: `afterDate expected nothing or number, got ${typeof (req.query.afterDate)} with value ${req.query.afterDate}`})
+    const logsRaw = await LogSchema.find({
+        timestamp: { $gte: parseInt(req.query.afterDate) }
+    }, {}, {limit: parseInt(req.query.limit)})
+    const logsFiltered = logsRaw.map((log) => {
+        log.apiKey = undefined
+        log.ip = undefined
+        if (log.responseBody && log.responseBody.key) log.responseBody.key = undefined
+        return log
+    })
+    res.status(200).json(logsFiltered)
 })
 
 
