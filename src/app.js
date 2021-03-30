1fbd3c8b160d534480139c19d810f3c173962786
oof2win2 <23482862+oof2win2@users.noreply.github.com>
2021-03-30 20:01:33 +0200

    Move of all endpoints to /v1/ for versioning later

--- src/app.js
+++ src/app.js
@@ -42,15 +42,15 @@ const authMiddleware = async (req, res, next) => {
         return res.status(410).send("AuthenticationError: IP adress whitelist mismatch")
     next()
 }
-app.use('*', authMiddleware)
+app.use('/v1/*', authMiddleware)
 
-app.use('/rules', ruleRouter)
-app.use('/communities', communityRouter)
-app.use('/violations', violationRouter)
-app.use('/revocations', revocationRouter)
-app.use('/informatics', informaticsRouter)
-app.use('/testing', testingRouter)
-app.use('/offenses', offenseRouter)
+app.use('/v1/rules', ruleRouter)
+app.use('/v1/communities', communityRouter)
+app.use('/v1/violations', violationRouter)
+app.use('/v1/revocations', revocationRouter)
+app.use('/v1/informatics', informaticsRouter)
+app.use('/v1/testing', testingRouter)
+app.use('/v1/offenses', offenseRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res) {
