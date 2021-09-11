60cfaaa89fd53740aca0da676094ffbfdb500a3e
oof2win2 <honza.koco44@gmail.com>
2021-09-11 21:30:47 +0200

    :sparkles: Better WebSocket messages

--- src/utils/info.ts
+++ src/utils/info.ts
@@ -9,6 +9,7 @@ import { BeAnObject } from "@typegoose/typegoose/lib/types"
 import { RuleClass } from "../database/fagc/rule.js"
 import { CommunityClass } from "../database/fagc/community.js"
 import { ReportClass } from "../database/fagc/report.js"
+import { CommunityCreatedMessageExtraOpts, CommunityRemovedMessageExtraOpts, ReportMessageExtraOpts, RevocationMessageExtraOpts } from "fagc-api-types"
 
 const wss = new WebSocket.Server({ port: ENV.WS_PORT })
 
@@ -63,51 +64,48 @@ export function WebsocketMessage(message: string): void {
 	})
 }
 
-interface ReportMessageOpts {
-	community: CommunityClass,
-	rule: DocumentType<RuleClass, BeAnObject>
-	admin: User
-	totalReports: number
-	totalCommunities: number
-}
-
-export async function reportCreatedMessage(report: DocumentType<ReportClass, BeAnObject>, opts: ReportMessageOpts): Promise<void> {
+export async function reportCreatedMessage(report: DocumentType<ReportClass, BeAnObject>, opts: ReportMessageExtraOpts): Promise<void> {
 	if (report === null || report.playername === undefined) return
 
 	// set the sent object's messageType to report
-	WebsocketMessage(JSON.stringify(Object.assign({}, report.toObject(), { messageType: "report" })))
+	// WebsocketMessage(JSON.stringify(Object.assign({}, report.toObject(), { messageType: "report" })))
 
 	const reportEmbed = new MessageEmbed()
 		.setTitle("FAGC - Report Created")
 		.setDescription(
-			`${report.automated ? "Automated " : ""}Report \`${report.id}\` created at <t:${Math.round(report.reportedTime.valueOf()/1000)}>\n` +
+			`${report.automated ? "Automated " : ""}Report \`${report.id}\` created at <t:${Math.round(report.reportedTime.valueOf() / 1000)}>\n` +
 			`${opts.totalReports} reports in ${opts.totalCommunities} communities`
-			)
+		)
 		.setColor("ORANGE")
 		.addFields(
 			{ name: "Playername", value: report.playername, inline: true },
 			{ name: "Broken Rule", value: `${opts.rule.shortdesc} (\`${opts.rule.id}\`)`, inline: true },
 			{ name: "Description", value: report.description, inline: false },
-			{ name: "Admin", value: `<@${opts.admin.id}> | ${opts.admin.tag}`, inline: true },
+			{ name: "Admin", value: `<@${opts.admin.id}> | ${opts.admin.username}#${opts.admin.discriminator}`, inline: true },
 			{ name: "Community", value: `${opts.community.name} (\`${opts.community.id}\`)`, inline: true },
 		)
 		.setTimestamp()
 	if (report.proof !== "No Proof") reportEmbed.addField("Proof", report.proof, false)
 	WebhookMessage(reportEmbed)
+
+	WebsocketMessage(JSON.stringify({
+		embed: reportEmbed,
+		report: report,
+		extraData: opts,
+		messageType: "report"
+	}))
 }
 
-interface RevocationMessageOpts extends ReportMessageOpts {
-	revokedBy: User
-}
-export async function reportRevokedMessage(revocation: DocumentType<RevocationClass, BeAnObject>, opts: RevocationMessageOpts): Promise<void> {
+export async function reportRevokedMessage(revocation: DocumentType<RevocationClass, BeAnObject>, opts: RevocationMessageExtraOpts): Promise<void> {
 	if (revocation === null || revocation.playername === undefined) return
 
 	// set the sent object's messageType to revocation
-	WebsocketMessage(JSON.stringify(Object.assign({}, revocation.toObject(), { messageType: "revocation" })))
+	// WebsocketMessage(JSON.stringify(Object.assign({}, revocation.toObject(), { messageType: "revocation" })))
+
 	const revocationEmbed = new MessageEmbed()
 		.setTitle("FAGC - Report Revoked")
 		.setDescription(
-			`${revocation.automated ? "Automated " : ""}Report \`${revocation.reportId}\` revoked with \`${revocation.id}\` at <t:${Math.round(revocation.revokedTime.valueOf()/1000)}>\n` +
+			`${revocation.automated ? "Automated " : ""}Report \`${revocation.reportId}\` revoked with \`${revocation.id}\` at <t:${Math.round(revocation.revokedTime.valueOf() / 1000)}>\n` +
 			`${opts.totalReports} reports in ${opts.totalCommunities} communities`
 		)
 		.setColor("#0eadf1")
@@ -115,20 +113,28 @@ export async function reportRevokedMessage(revocation: DocumentType<RevocationCl
 			{ name: "Playername", value: revocation.playername, inline: true },
 			{ name: "Broken Rule", value: `${opts.rule.shortdesc} (\`${opts.rule.id}\`)`, inline: true },
 			{ name: "Description", value: revocation.description, inline: false },
-			{ name: "Admin", value: `<@${opts.admin.id}> | ${opts.admin.tag}`, inline: true },
+			{ name: "Admin", value: `<@${opts.admin.id}> | ${opts.admin.username}#${opts.admin.discriminator}`, inline: true },
 			{ name: "Community", value: `${opts.community.name} (\`${opts.community.id}\`)`, inline: true },
-			{ name: "Revoked by", value: `<@${opts.revokedBy.id}> | ${opts.revokedBy.tag}`, inline: true },
+			{ name: "Revoked by", value: `<@${opts.revokedBy.id}> | ${opts.revokedBy.username}#${opts.revokedBy.discriminator}`, inline: true },
 		])
 		.setTimestamp()
 	if (revocation.proof !== "No Proof") revocationEmbed.addField("Proof", revocation.proof, false)
 	WebhookMessage(revocationEmbed)
+
+	WebsocketMessage(JSON.stringify({
+		embed: revocationEmbed,
+		revocation: revocation,
+		extraData: opts,
+		messageType: "revocation"
+	}))
 }
 
 export async function ruleCreatedMessage(rule: DocumentType<RuleClass, BeAnObject>): Promise<void> {
 	if (rule === null || rule.shortdesc === undefined || rule.longdesc === undefined) return
 
 	// set the sent object's messageType to ruleCreated
-	WebsocketMessage(JSON.stringify(Object.assign({}, rule.toObject(), { messageType: "ruleCreated" })))
+	// WebsocketMessage(JSON.stringify(Object.assign({}, rule.toObject(), { messageType: "ruleCreated" })))
+
 	const ruleEmbed = new MessageEmbed()
 		.setTitle("FAGC - Rule Created")
 		.setColor("#6f4fe3")
@@ -138,12 +144,19 @@ export async function ruleCreatedMessage(rule: DocumentType<RuleClass, BeAnObjec
 			{ name: "Rule long description", value: rule.longdesc, inline: true },
 		)
 	WebhookMessage(ruleEmbed)
+
+	WebsocketMessage(JSON.stringify({
+		messageType: "ruleCreated",
+		embed: ruleEmbed,
+		rule: rule,
+	}))
 }
 
 export async function ruleRemovedMessage(rule: DocumentType<RuleClass, BeAnObject>): Promise<void> {
 	if (rule === null || rule.shortdesc === undefined || rule.longdesc === undefined) return
 	// set the sent object's messageType to ruleRemoved
-	WebsocketMessage(JSON.stringify(Object.assign({}, rule.toObject(), { messageType: "ruleRemoved" })))
+	// WebsocketMessage(JSON.stringify(Object.assign({}, rule.toObject(), { messageType: "ruleRemoved" })))
+
 	const ruleEmbed = new MessageEmbed()
 		.setTitle("FAGC - Rule Removed")
 		.setColor("#6f4fe3")
@@ -153,34 +166,55 @@ export async function ruleRemovedMessage(rule: DocumentType<RuleClass, BeAnObjec
 			{ name: "Rule long description", value: rule.longdesc, inline: true },
 		)
 	WebhookMessage(ruleEmbed)
+
+	WebsocketMessage(JSON.stringify({
+		messageType: "ruleRemoved",
+		embed: ruleEmbed,
+		rule: rule,
+	}))
 }
 
-
-export async function communityCreatedMessage(community: DocumentType<CommunityClass, BeAnObject>, contact: User): Promise<void> {
+export async function communityCreatedMessage(community: DocumentType<CommunityClass, BeAnObject>, opts: CommunityCreatedMessageExtraOpts): Promise<void> {
 	// set the sent object's messageType to communityCreated
-	WebsocketMessage(JSON.stringify(Object.assign({}, community.toObject(), { messageType: "communityCreated" })))
+	// WebsocketMessage(JSON.stringify(Object.assign({}, community.toObject(), { messageType: "communityCreated" })))
+
 	const embed = new MessageEmbed()
 		.setTitle("FAGC - Community Created")
 		.setColor("#6f4fe3")
 		.addFields(
 			{ name: "Community ID", value: `\`${community.id}\``, inline: true },
 			{ name: "Community name", value: community.name, inline: true },
-			{ name: "Contact", value: `<@${contact.id}> | ${contact.tag}`, inline: true },
+			{ name: "Contact", value: `<@${opts.contact.id}> | ${opts.contact.username}#${opts.contact.discriminator}`, inline: true },
 		)
 	WebhookMessage(embed)
+
+	WebsocketMessage(JSON.stringify({
+		messageType: "communityCreated",
+		embed: embed,
+		community: community,
+		extraData: opts
+	}))
 }
-export async function communityRemovedMessage(community: DocumentType<CommunityClass, BeAnObject>, contact: User): Promise<void> {
+export async function communityRemovedMessage(community: DocumentType<CommunityClass, BeAnObject>, opts: CommunityRemovedMessageExtraOpts): Promise<void> {
 	// set the sent object's messageType to communityRemoved
-	WebsocketMessage(JSON.stringify(Object.assign({}, community.toObject(), { messageType: "communityRemoved" })))
+	// WebsocketMessage(JSON.stringify(Object.assign({}, community.toObject(), { messageType: "communityRemoved" })))
+
 	const embed = new MessageEmbed()
 		.setTitle("FAGC - Community Removed")
 		.setColor("#6f4fe3")
 		.addFields(
 			{ name: "Community ID", value: `\`${community.id}\``, inline: true },
 			{ name: "Community name", value: community.name, inline: true },
-			{ name: "Contact", value: `<@${contact.id}> | ${contact.tag}`, inline: true },
+			{ name: "Contact", value: `<@${opts.contact.id}> | ${opts.contact.username}#${opts.contact.discriminator}`, inline: true },
 		)
 	WebhookMessage(embed)
+	
+	WebsocketMessage(JSON.stringify({
+		messageType: "communityCreated",
+		embed: embed,
+		community: community,
+		extraData: opts
+	}))
 }
 export function communityConfigChanged(config: DocumentType<ConfigClass, BeAnObject>): void {
 	wss.clients.forEach(client => {
@@ -188,8 +222,8 @@ export function communityConfigChanged(config: DocumentType<ConfigClass, BeAnObj
 		const guildId = WebhookGuildIDs.get(client)
 		if (guildId == config.guildId) {
 			client.send(Buffer.from(JSON.stringify({
-				...config,
-				messageType: "guildConfig"
+				config: config,
+				messageType: "communityConfigChanged"
 			})))
 		}
 	})
